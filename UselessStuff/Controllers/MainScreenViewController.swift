f8aac72830cf3ac94d076bde783474256246a298
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-04-16 19:48:33 -0300

    Fixed

--- UselessStuff/Controllers/MainScreenViewController.swift
+++ UselessStuff/Controllers/MainScreenViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class MainScreenViewController: UIViewController {
-    @IBOutlet var itemButtons: [DraggableItem]!
+    @IBOutlet var draggableItems: [DraggableItem]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        checkItemsPosition()
+    }
+
+    func checkItemsPosition() {
+        let defaults = UserDefaults.standard
+
+        for draggableItem in draggableItems {
+            if let positionArray = defaults.array(forKey: draggableItem.item.rawValue) {
+                if let pos = positionArray as? [CGFloat] {
+                    let origin = CGPoint(x: pos[0], y: pos[1])
+                    draggableItem.frame = CGRect(origin: origin, size: draggableItem.frame.size)
+                }
+            }
+        }
     }
 
     @IBAction func itemTouched(_ draggableItem: DraggableItem) {
--- UselessStuff/Views/DraggableItem.swift
+++ UselessStuff/Views/DraggableItem.swift
@@ -28,12 +28,31 @@ class DraggableItem: UIButton {
 
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesCancelled(touches, with: event)
+
         if let first = touches.first {
             let pos = first.location(in: superview)
             let x = pos.x - self.frame.width / 2
             let y = pos.y - self.frame.height / 2
             let origin = CGPoint(x: x, y: y)
             self.frame = CGRect(origin: origin, size: self.frame.size)
+
+//            let defaults = UserDefaults.standard
+//            defaults.synchronize()
+//            defaults.setValue([x, y], forKey: item.rawValue)
+//            defaults.synchronize()
+        }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+
+        if let first = touches.first {
+            let pos = first.location(in: superview)
+            let x = pos.x - self.frame.width / 2
+            let y = pos.y - self.frame.height / 2
+
+            let defaults = UserDefaults.standard
+            defaults.setValue([x, y], forKey: item.rawValue)
         }
     }
 }
