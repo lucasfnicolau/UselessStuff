155e0dcb6dd098efd8589cc1f02cd67e0eec4985
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-04-16 17:33:31 -0300

    Initial Commit

---
+++ UselessStuff/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UselessStuff
+//
+//  Created by Lucas Fernandez Nicolau on 16/04/20.
+//  Copyright © 2020 Lucas Fernandez Nicolau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
